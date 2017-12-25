c163c2da11f8d3c9a2fa232ae4e358da9b26a2e0
bibim18 <namfon_inuyasha@hotmail.com>
2017-12-25 15:15:52 +0700

    second edit

--- testJS.js
+++ testJS.js
@@ -1,9 +1,16 @@
 const koa = require('koa');
 const Route = require('koa-router');
 const Sequelize = require('sequelize');
+const bodyParser = require('koa-bodyparser')
 
 const app = new koa()
 const router = new Route()
+
+app.use(bodyParser())
+app.use(router.allowedMethods())
+app.use(router.routes())
+
+
 
 let users;
 
@@ -11,18 +18,47 @@ const sequelize = new Sequelize('it', 'root', '12345678', {
     dialect: 'mysql'
   })
 
-  sequelize.query("SELECT * FROM test", {type: Sequelize.QueryTypes.SELECT}).then(myTableRows => {
-    users = myTableRows;
-  })
+//   sequelize.query("SELECT * FROM test", {type: Sequelize.QueryTypes.SELECT}).then(myTableRows => {
+//     users = myTableRows;
+//   })
 
- // /user/:id
-router.get('/user', ctx => {
-    ctx.body = users;
+ const test = sequelize.define('test', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    fname: {
+      type: Sequelize.STRING(20),
+      allowNull: true
+    },
+    lname: {
+      type: Sequelize.STRING(30),
+      allowNull: true
+    }
+  }, {
+    tableName: 'test',
+    timestamp: false
+  }) 
+
+  const Op = Sequelize.Op;
+
+//select * from test where id = 2
+router.get('/test', async(ctx) => {
+  let data = await test.findAll()
+  ctx.body = data
 })
 
-app.use(router.allowedMethods())
-app.use(router.routes())
-app.use(require('koa-body')())
+//insert into test
+router.post('/test', async(ctx) => {
+  // console.log(ctx.request.body)
+  const {fname, lname} = ctx.request.body
+  console.log(fname, lname)
+  let data = await test.create({ fname, lname })
+  ctx.body = data
+})
+
+
 
 const port = 3001
 app.listen(port, () => {
